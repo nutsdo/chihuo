da977a93c71d35da0ba8b57cb62533b9288c58d6
nutsdo <ldingtao@163.com>
2014-11-14 17:33:36 +0800

    2014.11.14
    css

--- app/views/template/default/search.blade.php
+++ app/views/template/default/search.blade.php
@@ -20,28 +20,30 @@
           <div class="masthead clearfix">
             <div class="inner">
               <h3 class="masthead-brand">吃货小队</h3>
-              <ul class="nav masthead-nav">
-                <li class="active"><a href="#">首页</a></li>
-              </ul>
             </div>
           </div>
 
           <div class="inner cover">
-			{{ Form::open(array(
+			<div class="row">
+			  <div class="col-lg-12">
+			  {{ Form::open(array(
 				   'route' => 'search_show',
 				   'id' => 'search',
-				   'class' => 'form-inline', 
 				   'role' => 'form',
-			))}}
-			<div class="form-group" style="width:80%;">
-				<label class="sr-only" for="exampleInputEmail2">关键字</label>
-				{{Form::text('keywords','',array('class'=>"form-control",'style'=>"width:100%",'placeholder'=>"请输入关键字"))}}			
+			  ))}}
+			    <div class="input-group">
+			      {{Form::text('keywords','',array('class'=>"form-control",'placeholder'=>"请输入关键字"))}}	
+			      <span class="input-group-btn">
+			        {{Form::submit('搜索',array('class'=>'btn btn-default'))}}
+			      </span>
+			    </div>
+			    {{Form::close()}}
+			  </div>
 			</div>
-			{{Form::submit('搜索',array('class'=>'btn btn-default'))}} 
-			{{Form::close()}}
+						
           </div>
 
-          <div class="mastfoot">
+          <div class="mastfoot navbar-fixed-bottom">
             <div class="inner">
               <p>工信部备案： 冀ICP备13015451号-1 ©2014  <a href="http://www.chihuo1408.com">吃货小队</a> 版权所有, by <a href="http://www.chihuo1408.com">@Mr.lv</a>.</p>
             </div>
