13cf6299108285453e2ea6fafd86b912be5b92ce
nutsdo <ldingtao@163.com>
2014-11-25 09:01:17 +0800

    20141125
    统计代码

--- app/views/layouts/chihuo.blade.php
+++ app/views/layouts/chihuo.blade.php
@@ -141,5 +141,10 @@ $(function (){
 	}
 
 </script>
+<script type="text/javascript">
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F94ccdbb89d6de952b1e7eff952dfcde6' type='text/javascript'%3E%3C/script%3E"));
+</script>
+
 </body>
 </html>
