ccebf59e8864bcf64464eddddda9607a994e830b
nutsdo <ldingtao@163.com>
2014-12-02 17:04:11 +0800

    333

--- app/Api/weixin/Weixin.php
+++ app/Api/weixin/Weixin.php
@@ -15,7 +15,7 @@ class Weixin {
     public function responseMsg()
     {
 		//get post data, May be due to the different environments
-		$postStr = isset($GLOBALS["HTTP_RAW_POST_DATA"])?$GLOBALS["HTTP_RAW_POST_DATA"]:'';
+		$postStr = file_get_contents("php://input");
 
       	//extract post data
 		if (!empty($postStr)){
