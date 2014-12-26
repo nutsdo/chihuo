19259347d67d0831f955a2cdc8e164d16051197d
nutsdo <ldingtao@163.com>
2014-12-26 16:45:53 +0800

    20141226
    自动回复7

--- app/controllers/WechatController.php
+++ app/controllers/WechatController.php
@@ -22,39 +22,39 @@ class WechatController extends BaseController{
 	}
 	//自定义回复
 	public function store(){
-		return $this->responseMsg();
-// 		$message = file_get_contents('php://input');
-//     	$message = simplexml_load_string($message, 'SimpleXMLElement', LIBXML_NOCDATA);
+		//return $this->responseMsg();
+		$message = file_get_contents('php://input');
+    	$message = simplexml_load_string($message, 'SimpleXMLElement', LIBXML_NOCDATA);
     	
-//     	$type='text';
-//     	$content='欢迎关注吃货小队';
+    	$type='text';
+    	$content='欢迎关注吃货小队';
     	
-//     	//查询数据库，匹配关键字
-//     	$keyword = $message->Content;
-//     	$tags = Tag::where('name','=',$keyword)->first();
-//     	if(empty($tags)){
-//     		$type=='text';
-//     		$content = '请输入精准的关键字^ ^';
-//     		return View::make('weixin.index')->with('message',$message)
-//     										 ->with('content',$content)
-//     										 ->with('type',$type);
-//     	}else{
-//     		$posts = Tag::find($tags->tag_id)->posts;
-//     		if(!empty($posts)){
-//     			$type=="news";
-//     			$count = count($posts);
-//     			return View::make('weixin.news')->with('posts', $posts)
-// 									    			->with('message',$message)
-// 									    			->with('count',$count)
-//     												->with('type',$type);
-//     		}else {
-//     			$type=='text';
-//     			$content = '请输入精准的关键字^ ^';
-//     			return View::make('weixin.index')->with('message',$message)
-//     												->with('content',$content)
-//     												->with('type',$type);
-//     		}
-//     	}
+    	//查询数据库，匹配关键字
+    	$keyword = $message->Content;
+    	$tags = Tag::where('name','=',$keyword)->first();
+    	if(empty($tags)){
+    		$type=='text';
+    		$content = '请输入精准的关键字^ ^';
+    		return View::make('weixin.index')->with('message',$message)
+    										 ->with('content',$content)
+    										 ->with('type',$type);
+    	}else{
+    		$posts = Tag::find($tags->tag_id)->posts;
+    		if(!empty($posts)){
+    			$type=="news";
+    			$count = count($posts);
+    			return View::make('weixin.news')->with('posts', $posts)
+									    			->with('message',$message)
+									    			->with('count',$count)
+    												->with('type',$type);
+    		}else {
+    			$type=='text';
+    			$content = '请输入精准的关键字^ ^';
+    			return View::make('weixin.index')->with('message',$message)
+    												->with('content',$content)
+    												->with('type',$type);
+    		}
+    	}
 	}
 	
 	//响应消息
